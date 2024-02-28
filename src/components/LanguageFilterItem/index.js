c3c68e139f51bf7cf29dff83cc36e07fb502f6e9
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-28 11:37:42 +0000

    filter item added

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,1 +1,25 @@
-// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {repoDetails, isSame, filterRepositories} = props
+  const {id, language} = repoDetails
+  const changeClass = isSame ? 'selected' : 'not-selected'
+
+  const onClickedFilterItem = () => {
+    filterRepositories(id)
+  }
+
+  return (
+    <li className="filter-item">
+      <button
+        type="button"
+        className={changeClass}
+        onClick={onClickedFilterItem}
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
